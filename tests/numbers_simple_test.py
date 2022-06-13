092756ac02b2b0c80d4afdf5a926f8a9a342850c
Timur Samkharadze <timur.samkharadze@cognitedata.com>
2022-06-13 14:44:18 +0200

    added benchmark for tests

--- tests/numbers_simple_test.py
+++ tests/numbers_simple_test.py
@@ -1,8 +1,10 @@
+import pytest
 from validator import Validator
 import sys
 sys.path.append("..")
 
 
+@pytest.mark.benchmark(warmup_iterations=1000, min_time=0.5,max_time=1,min_rounds=3, warmup=True)
 class TestNumbersShouldPass:
 
     data = {
@@ -16,65 +18,62 @@ class TestNumbersShouldPass:
         }
     }
 
-    def test_eq_0(self):
+    def validate(self, benchmark, rule):
+        validator = Validator(rule)
+        assert len(benchmark(validator.validate, obj=self.data)) == 0
+
+    def test_eq_0(self, benchmark):
         rule = '''
                 [
                     ["eq", 1, 1]
                 ]
             '''
-        validator = Validator(rule)
-        assert len(validator.validate(self.data)) == 0
+        self.validate(benchmark, rule)
 
-    def test_eq_1(self):
+    def test_eq_1(self, benchmark):
         rule = '''
                 [
                     ["eq", "$.foo", 1]
                 ]
             '''
-        validator = Validator(rule)
-        assert len(validator.validate(self.data)) == 0
+        self.validate(benchmark, rule)
 
-    def test_eq_2(self):
+    def test_eq_2(self, benchmark):
         rule = '''
                 [
                     ["eq", "$.foo", "$.bar"]
                 ]
             '''
-        validator = Validator(rule)
-        assert len(validator.validate(self.data)) == 0
+        self.validate(benchmark, rule)
 
-    def test_eq_3(self):
+    def test_eq_3(self, benchmark):
         rule = '''
                 [
                     ["eq", "$.bar", "$.foo"]
                 ]
             '''
-        validator = Validator(rule)
-        assert len(validator.validate(self.data)) == 0
+        self.validate(benchmark, rule)
 
-    def test_eq_4(self):
+    def test_eq_4(self, benchmark):
         rule = '''
                 [
                     ["eq", "$.foo", "$.baz.foo.bar"]
                 ]
             '''
-        validator = Validator(rule)
-        assert len(validator.validate(self.data)) == 0
+        self.validate(benchmark, rule)
 
-    def test_eq_5(self):
+    def test_eq_5(self, benchmark):
         rule = '''
                 [
                     ["eq", "$.foo", "$.foo"]
                 ]
             '''
-        validator = Validator(rule)
-        assert len(validator.validate(self.data)) == 0
+        self.validate(benchmark, rule)
 
-    def test_eq_6(self):
+    def test_eq_6(self, benchmark):
         rule = '''
                 [
                     ["not", ["eq", "$.foo", "$.baz.foo.baz"]]
                 ]
             '''
-        validator = Validator(rule)
-        assert len(validator.validate(self.data)) == 0
+        self.validate(benchmark, rule)
